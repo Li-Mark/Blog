d8cb3241eed7c5c5a069df7f114321b7e31a6033
Yunlong LI <yli135@slb.com>
2019-04-22 17:02:54 +0800

    Add markdown

--- blog/src/app/app.module.ts
+++ blog/src/app/app.module.ts
@@ -1,4 +1,5 @@
 import { BrowserModule } from "@angular/platform-browser";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 import { NgModule } from "@angular/core";
 
 import { AppRoutingModule } from "./app-routing.module";
@@ -6,8 +7,13 @@ import { AppComponent } from "./app.component";
 import { MarkdownModule } from "ngx-markdown";
 
 @NgModule({
-  declarations: [AppComponent, MarkdownModule.forRoot()],
-  imports: [BrowserModule, AppRoutingModule],
+  declarations: [AppComponent],
+  imports: [
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    BrowserModule,
+    MarkdownModule.forRoot()
+  ],
   providers: [],
   bootstrap: [AppComponent]
 })
