0742cdb0fdaac46fab123f01054cce5f48af38b0
liyunlong <liyunlong_haerbin@hotmail.com>
2020-04-03 14:59:52 +0800

    chore(release): update to angular9

--- src/app/profile/profile.module.ts
+++ src/app/profile/profile.module.ts
@@ -3,11 +3,11 @@ import { CommonModule } from "@angular/common";
 import { MatIconModule } from "@angular/material/icon";
 import { ProfileComponent } from "./profile.component";
 import { SkillModule } from "./skill/skill.module";
-import { MarkdownModule } from 'ngx-markdown';
+import { MarkdownModule } from "ngx-markdown";
 
 @NgModule({
   declarations: [ProfileComponent],
   exports: [ProfileComponent],
   imports: [CommonModule, MatIconModule, SkillModule, MarkdownModule.forChild()]
 })
-export class ProfileModule {}
+export class ProfileModule { }
